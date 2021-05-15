1af8940e970806378511ca4d15d07c6e745194e4
Spinal29 <ricardoespinal@hotmail.com>
2021-05-14 18:26:41 -0600

    CarritoV3

--- app/src/main/java/sv/edu/udb/proyectodsm/CartActivity.java
+++ app/src/main/java/sv/edu/udb/proyectodsm/CartActivity.java
@@ -2,13 +2,23 @@ package sv.edu.udb.proyectodsm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class CartActivity extends AppCompatActivity {
+
+    TextView txtOrden;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
+
+        Intent data = getIntent();
+        String orden = data.getStringExtra("orden");
+
+        txtOrden=findViewById(R.id.txtOrden);
+        txtOrden.setText(orden);
     }
 }
